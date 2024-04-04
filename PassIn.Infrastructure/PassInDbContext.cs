50b00c8ce6fc2f11066ce630f40a1d57f1c99b7a
Diogo Padoveis Antunes <103612298+dpadoveis@users.noreply.github.com>
2024-04-04 14:42:51 -0300

    Update PassInDbContext.cs

--- PassIn.Infrastructure/PassInDbContext.cs
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -6,11 +6,9 @@ namespace PassIn.Infrastructure
 {
     public class PassInDbContext : DbContext
     {
-        //variavel p mapear e acessar a tabela de eventos no db
         public DbSet<Event> Events { get; set; }
         public DbSet<Attendee> Attendees { get; set; }
         public DbSet<CheckIn> CheckIns { get; set; }
-        //metodo p configurar a database do sqlite
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source = C:\\Users\\Diogo\\source\\repos\\rocketseat\\PassIn\\PassInDb.db");
