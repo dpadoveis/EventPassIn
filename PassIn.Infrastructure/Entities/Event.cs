dd2e5198bdca33bf3577733466421c8f4c658e5b
Diogo Padoveis Antunes <103612298+dpadoveis@users.noreply.github.com>
2024-04-04 14:34:52 -0300

    Add files via upload

---
+++ PassIn.Infrastructure/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassIn.Infrastructure.Entities
+{
+    //classe pra instanciar elementos, com as específicações do db
+    public class Event
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+        public int Maximum_Attendees { get; set; }
+        [ForeignKey("Event_Id")]
+        public List<Attendee> Attendees { get; set; } = [];
+    }
+}
